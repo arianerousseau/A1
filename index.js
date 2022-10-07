68886bcc2517df86c010d0702696cc72e9bb09b9
arianerousseau <ariane.rousseau@efrei.net>
2022-10-06 21:14:04 -0700

    New change on index.js and package.json

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("Welcome to the first assignment !");
+});
+
+app.listen(3000, function(){
+    console.log('App assignment 1 listening on port 3000!')
+});
